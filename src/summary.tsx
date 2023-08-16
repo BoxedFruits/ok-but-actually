5136ead55a568b043af85e902e3978f1d19c0b9c
BoxedFruits <34636700+BoxedFruits@users.noreply.github.com>
2023-08-15 21:47:10 -0400

    refactor

--- src/summary.tsx
+++ src/summary.tsx
@@ -9,10 +9,24 @@ import "./summary.css";
 console.log("content script loaded", document.readyState);
 
 window.addEventListener("load", function load(event) {
-  console.log("All resources finished loading!");
-  console.log(document.readyState)
-
   // window.removeEventListener("load", load, false);
+  const jobTitleElm = document.querySelector(".display-flex.justify-space-between.flex-wrap")
+  createSummaryContainer(jobTitleElm, parseYearsOfExperience())
+}, { once: true });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => { //this might create be a race condition with onMessage vs page load 
+  // console.log(message, " from chrome runtime")
+  // console.log(document.readyState)
+  const summaryContainer = document.querySelector(".summary-container")
+  if (summaryContainer) {
+    summaryContainer.remove()
+  }
+
+  const jobTitleElm = document.querySelector(".display-flex.justify-space-between.flex-wrap")
+  createSummaryContainer(jobTitleElm, parseYearsOfExperience())
+})
+
+const parseYearsOfExperience = () => {
   const regex = /(\d+)(?:\s*-\s*(\d+))?\s*\+?\s*years?/g;
   //@ts-ignore For linkedIn, it will always be there
   const text = document.querySelector(".jobs-description-content__text").innerText
@@ -29,46 +43,13 @@ window.addEventListener("load", function load(event) {
     }
   });
 
-  console.log(yearsOfExperience);
-}, { once: true });
+  return yearsOfExperience[0]
+}
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => { //this might create be a race condition with onMessage vs page load 
-  console.log(message, " from chrome runtime")
-  console.log(document.readyState)
-  const regex = /(\d+)(?:\s*-\s*(\d+))?\s*\+?\s*years?/g;
-  //@ts-ignore For linkedIn, it will always be there
-  const text = document.querySelector(".jobs-description-content__text").innerText
-  const summaryContainer = document.querySelector(".summary-container")
-
-  if (summaryContainer) {
-    summaryContainer.remove()
-  }
-
-  // console.log(text)
-  const matches = [...text.matchAll(regex)];
-  const yearsOfExperience = matches.map(match => {
-    const rangeMin = match[1];
-    const rangeMax = match[2];
-
-    if (rangeMax !== undefined) {
-      return Math.min(Number(rangeMin), Number(rangeMax));
-    } else {
-      return Number(rangeMin);
-    }
-  });
-  console.log(yearsOfExperience);
-  sendResponse(`yuh ${text === "About the job"}`)
-  const jobTitleElm = document.querySelector(".display-flex.justify-space-between.flex-wrap")
-  // console.log(jobTitleElm)
-
-  // jobTitleElm?.insertAdjacentHTML("afterend","<div class='summary-container'></div>")
-  createSummaryContainer(jobTitleElm, yearsOfExperience)
-})
-
-const createSummaryContainer = (jobTitleElm: Element | null, yearsOfExperience) => {
+const createSummaryContainer = (jobTitleElm: Element | null, yearsOfExperience: number) => {
   const getYoe = () => {
-    if (yearsOfExperience.length !== 0) {
-      return <div className="badge">Minimum of {yearsOfExperience[0]} years of experience</div>
+    if (yearsOfExperience !== undefined) {
+      return <div className="badge">Minimum of {yearsOfExperience} years of experience</div>
     } else {
       return <div className="badge">Couldn't retrieve minimum years of experience</div>
     }
